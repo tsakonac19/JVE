d0cb00d41309dac37aecfa246ff427f6428c0af0
tsakonac19 <161424018+tsakonac19@users.noreply.github.com>
2024-03-01 10:48:19 +0200

    Create MainClass.java

---
+++ classes/MainClass.java
@@ -0,0 +1,11 @@
+package helloproject;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Good morning, Earth!");
+		// Person p1 = new Person("Mpampis", "Peribolaris");
+		// p1.print();
+		Student s1 = new Student("Mpampis", "Mpampi", "G3", 64772293);
+		s1.print();
+	}
+}
